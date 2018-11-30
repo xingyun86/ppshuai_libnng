bd501363b7195585e013893ff91eb281f4e39778
xingyun86 <xingyun_ppshuai@yeah.net>
2018-11-30 21:03:14 +0800

    提交nanomsg的升级版nng，支持更多协议及错误处理，windows下32位和64位工程

---
+++ src/nng/vcprojects/docs/man/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/DevelopmentEnvironment/Workspaces/vs-workspace/nng/nng/docs/man
+# Build directory: D:/DevelopmentEnvironment/Workspaces/vs-workspace/nng/vcprojects/docs/man
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
